8d19299a46586a72b80b0eb8c917ee0bb1333ebc
Igor Loboda <i.loboda2024@gmail.com>
2026-03-28 22:23:24 +0200

    added iam user for cicd

---
+++ infra/setup/iam.tf
@@ -0,0 +1,53 @@
+# Create the IAM user and policies for CD
+resource "aws_iam_user" "cd" {
+  name = "recipe-app-api-cd"
+}
+
+# Machine to machine communication, the user that connects from GitHub and GitLlab to perform actions
+resource "aws_iam_access_key" "cd" {
+  user = aws_iam_user.cd.name
+}
+
+# Policy for S3 terraform backend to s3 and DynamoDB access
+# Allow the list bucket permission on this specific bucket and what's needed for our CD account to be able to access
+# the s3 bucket that contains TF state
+
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    # fine grained permissions, specific file location in a bucket, tf-state-deploy is the key
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-depley-env/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
+  }
+}
+
+resource "aws_iam_policy" "tf_backend" {
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  description = "Allow user to use s3 and DynamoDB for TF backend resources"
+  policy      = data.aws_iam_policy_document.tf_backend.json
+}
+
+resource "aws_iam_user_policy_attachment" "tf_backend" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.tf_backend.arn
+}
