a9fb7adeeb2a5d52a20f17730fc19ea407c2bab5
Igor Loboda <i.loboda2024@gmail.com>
2026-04-04 06:12:06 +0300

    added rds

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -13,14 +13,14 @@ variable "contact" {
   default     = "mark@example.com"
 }
 
-# variable "db_username" {
-#   description = "Username for the recipe app api database"
-#   default     = "recipeapp"
-# }
+variable "db_username" {
+   description = "Username for the recipe app api database"
+   default     = "recipeapp"
+}
 
-# variable "db_password" {
-#   description = "Password for the Terraform database"
-# }
+variable "db_password" {
+   description = "Password for the Terraform database"
+}
 
 variable "ecr_proxy_image" {
   description = "Path to the ECR repo with the proxy image"
@@ -31,7 +31,7 @@ variable "ecr_app_image" {
 }
 
 variable "django_secret_key" {
-   description = "Secret key for Django"
+  description = "Secret key for Django"
 }
 
 # variable "dns_zone_name" {
