d2c38985008fe73e37cccfc61f34b74510b1db3f
Igor Loboda <i.loboda2024@gmail.com>
2026-04-05 18:12:25 +0300

    add ecs service

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -53,23 +53,55 @@ resource "aws_ecs_task_definition" "api" {
   task_role_arn            = aws_iam_role.app_task.arn
 
   container_definitions = jsonencode([
-    # {
-    #   name = "api"
-    #   image = var.ecr_app_image
-    #   essential = true
-    #   memoryReservation = 256
-    #   user = "django-user"
-    #   environment = [
-    #     {
-    #       name = "DJANGO_SECRET_KEY"
-    #       value = var.django_secret_key
-    #     },
-    #     {
-    #       name = "DB_HOST"
-    #       value =
-    #     }
-    #   ]
-    # },
+    {
+      name              = "api"
+      image             = var.ecr_app_image
+      essential         = true
+      memoryReservation = 256
+      user              = "django-user"
+      environment = [
+        {
+          name  = "DJANGO_SECRET_KEY"
+          value = var.django_secret_key
+        },
+        {
+          name  = "DB_HOST"
+          value = aws_db_instance.main.address
+        },
+        {
+          name  = "DB_NAME"
+          value = aws_db_instance.main.db_name
+        },
+        {
+          name  = "DB_USER"
+          value = aws_db_instance.main.username
+        },
+        {
+          name  = "DB_PASS"
+          value = aws_db_instance.main.password
+        },
+        {
+          name  = "ALLOWED_HOSTS"
+          value = "*"
+        }
+      ]
+      mountPoints = [
+        {
+          readOnly      = false
+          containerPath = "/vol/web/static"
+          sourceVolume  = "static"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
+          awslogs-region        = data.aws_region.current.name
+          awslogs-stream-prefix = "api"
+        }
+      }
+
+    },
     {
       name              = "proxy"
       image             = var.ecr_proxy_image
@@ -116,3 +148,60 @@ resource "aws_ecs_task_definition" "api" {
   }
 }
 
+resource "aws_security_group" "ecs_service" {
+  description = "Access rules for the ecs service"
+  name        = "${local.prefix}-ecs-service"
+  vpc_id      = aws_vpc.main.id
+  # outbound access to endpoints
+  # Outbound access to endpoints
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # RDS connectivity
+  egress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
+  # HTTP inbound access for proxy app
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_service" "api" {
+  name            = "${local.prefix}-api"
+  cluster         = aws_ecs_cluster.main.name
+  task_definition = aws_ecs_task_definition.api.family
+  # number of running instances in our service
+  # if you add more it will increase cost but it gives the ability to process more requests and scale up your app
+  desired_count    = 1
+  launch_type      = "FARGATE"
+  platform_version = "1.4.0"
+  # allow for exec command
+  enable_execute_command = true
+
+  network_configuration {
+    # for tests only, connect to from public
+    assign_public_ip = true
+
+    subnets = [
+      aws_subnet.public_a.id,
+      aws_subnet.public_b.id
+    ]
+
+    security_groups = [aws_security_group.ecs_service.id]
+  }
+}
